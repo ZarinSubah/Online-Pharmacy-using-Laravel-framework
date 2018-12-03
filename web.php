81b902f9d784bb751f695e14be6a4db293ec3fc9
Sharlene Rozario <sharleneerozario@gmail.com>
2018-12-03 22:27:01 +0600

    Logging into admin panel
    for migration change needed only in 'public function up'

--- web.php
+++ web.php
@@ -27,3 +27,4 @@ Route::get('/','HomeController@index');
 //backend routes.......................
 Route::get('/admin','AdminController@index');
 Route::get('/dashboard','AdminController@show_dashboard');
+Route::post('/admin-dashboard','AdminController@dashboard');
