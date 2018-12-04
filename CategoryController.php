72063143af67f7d31e7132b4b9d976b83d49a083
ZarinSubah <zarin.subah02@northsouth.edu>
2018-12-04 19:17:13 +0600

    Category Controller edited fro save_category
    Create new category by admin and save those category

--- CategoryController.php
+++ CategoryController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+use Session;
+use Illuminate\Support\Facades\Redirect;
+session_start();
 
 class CategoryController extends Controller
 {
@@ -10,4 +15,26 @@ class CategoryController extends Controller
     {
         return view('admin.add_category');
     }
+     public function all_category()
+    {
+        return view('admin.all_category');
+    }
+    public function save_category(Request $request)
+    {
+        $data=array();
+        $data['category_id']=$request->category_id;
+        $data['category_name']=$request->category_name;
+        $data['category_description']=$request->category_description;
+        $data['publication_status']=$request->publication_status;
+
+        echo "<pre>";
+        print_r($data);
+        echo "</pre>";
+        //DB::table('category')->insert($data);
+        //Session::put('message','Category added successfully')
+        //return Redirect::('/add-category');
+
+
+    }
+
 }
