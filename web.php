9c3b05c16e02051adad156bf1a8f098c1fda7478
ZarinSubah <zarin.subah02@northsouth.edu>
2018-12-04 19:11:03 +0600

    Update web.php
    edited for save _category

--- web.php
+++ web.php
@@ -29,3 +29,9 @@ Route::get('/admin','AdminController@index');
 Route::get('/dashboard','AdminController@show_dashboard');
 Route::post('/admin-dashboard','AdminController@dashboard');
 Route::get('/logout','SuperAdminController@logout'); 
+
+//category related routes
+Route::get('/add-category','CategoryController@index'); 
+Route::get('/all-category','CategoryController@all_category'); 
+Route::post('/save-category','CategoryController@save_category'); 
+
