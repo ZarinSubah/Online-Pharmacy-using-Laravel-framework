996ed27512c44938142024d24285365267a69725
ZarinSubah <zarin.subah02@northsouth.edu>
2018-12-09 22:45:00 +0600

    Security assurance for admin panel

--- ManufactureController.php
+++ ManufactureController.php
@@ -11,10 +11,12 @@ class ManufactureController extends Controller
 {
     public function index()
     {
-      return view('admin.add_manufacture');
+      $this->AdminAuthCheck(); 
+        return view('admin.add_manufacture');
     }
     public function save_manufacture(Request $request)
-    {
+    {   
+        $this->AdminAuthCheck();  
       $data=array();
         $data['manufacture_id']=$request->manufacture_id;
         $data['manufacture_name']=$request->manufacture_name;
@@ -29,6 +31,7 @@ class ManufactureController extends Controller
  public function all_manufacture()
     {
         
+       $this->AdminAuthCheck(); 
        $all_manufacture_info=DB::table('tbl_manufacture')->get();
        $manage_manufacture=view('admin.all_manufacture')
            ->with('all_manufacture_info',$all_manufacture_info);
@@ -39,6 +42,7 @@ class ManufactureController extends Controller
 // Delete brands name from admin panel    
 public function delete_manufacture($manufacture_id)
     {
+        $this->AdminAuthCheck(); 
         DB::table('tbl_manufacture')
             ->where('manufacture_id',$manufacture_id)
             ->delete();
@@ -49,6 +53,7 @@ public function delete_manufacture($manufacture_id)
 //edit any brands infomartion    
 public function edit_manufacture($manufacture_id)
     {
+         $this->AdminAuthCheck(); 
          $manufacture_info=DB::table('tbl_manufacture')
                          ->where('manufacture_id',$manufacture_id)
                          ->first();
@@ -63,6 +68,7 @@ public function edit_manufacture($manufacture_id)
 // save edited information
 public function update_manufacture(Request $request,$manufacture_id)
     {
+         $this->AdminAuthCheck(); 
          $data=array();
          $data['manufacture_name']=$request->manufacture_name;
          $data['manufacture_description']=$request->manufacture_description;
@@ -72,7 +78,17 @@ public function update_manufacture(Request $request,$manufacture_id)
              ->update($data);
              Session::get('message','Manufacture update successfully !');
              return Redirect::to('/all-manufacture');
-    }        
+    } 
+    public function AdminAuthCheck()
+    {
+      $admin_id=Session::get('admin_id');
+      
+      if ($admin_id) {
+         return;
+      }else{
+         return Redirect::to('/admin')->send();
+      }
+    }       
 
 
 }
